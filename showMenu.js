eb69dd6b887b8b78900d4f983ffda640faa9e15d
david-abramov <sendtoabramov@gmail.com>
2021-12-11 02:53:19 +0300

    add project

---
+++ showMenu.js
@@ -0,0 +1,17 @@
+const mainMenu = document.querySelector(".main_menu");
+const btnMenu = document.querySelector(".header_menu");
+
+const openCloseMenu = () => {
+  const showMenu = () => {
+    mainMenu.style.display = "block";
+  };
+  btnMenu.addEventListener("click", showMenu);
+
+  document.onclick = function (event) {
+    if (event.target.className != "header_menu") {
+      mainMenu.style.display = "none";
+    }
+  };
+};
+
+openCloseMenu();
